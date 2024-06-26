69d3e1f33b1be5e27b10dec659553c80dc75b81e
lucasmonsan <lucasmonsan@gmail.com>
2024-06-26 13:19:44 -0300

    Projeto limpo

---
+++ src/pages/WelcomePage.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react"
+interface WelcomePageProps {}
+
+export const WelcomePage: FC<WelcomePageProps> = (props) => {
+  return (
+    <main>
+      <h1>WelcomePage</h1>
+    </main>
+  )
+}
