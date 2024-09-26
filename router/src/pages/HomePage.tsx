7cceb19bfc8e576c7c8e308f97a04abb3b889950
lucasmonsan <lucasmonsan@gmail.com>
2024-09-26 10:21:32 -0300

    Projeto com router já configurado.

---
+++ router/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+interface HomePageProps { };
+
+export const HomePage: FC<HomePageProps> = (props) => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+    </div>
+  );
+}
